01ac9177dde9f366b56592dbfd5d393f2b860e14
loihv <hvlepu@gmail.com>
2024-02-22 23:11:27 +0700

    video44

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,6 +8,16 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World from Loi!";
+        return "Hello World from Loi1!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page!!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page!!!";
     }
 }
